85102e2481ed155c78d8e4ad48580be59657cd7c
Denis Lebedev <d2.lebedev@gmail.com>
2015-08-16 18:06:34 +0100

    Hack Safari defaults

--- osx/set-defaults.sh
+++ osx/set-defaults.sh
@@ -75,6 +75,16 @@ defaults write com.apple.finder WarnOnEmptyTrash -bool false
 # Safari
 # ======
 
+# Privacy: don’t send search queries to Apple
+defaults write com.apple.Safari UniversalSearchEnabled -bool false
+defaults write com.apple.Safari SuppressSearchSuggestions -bool true
+
+# Show the full URL in the address bar (note: this still hides the scheme)
+defaults write com.apple.Safari ShowFullURLInSmartSearchField -bool true
+
+# Disable Safari’s thumbnail cache for History and Top Sites
+defaults write com.apple.Safari DebugSnapshotsUpdatePolicy -int 2
+
 # Set Safari’s home page to `about:blank` for faster loading
 defaults write com.apple.Safari HomePage -string "about:blank"
 
