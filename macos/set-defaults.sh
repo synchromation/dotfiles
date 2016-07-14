32643916d2850dbb5a95399882a06038464d88c6
Derek Michael Frank <derekmfrank@gmail.com>
2016-07-13 18:08:43 -0700

    refactor(macos): Update osx to macos
    Apple is changing their desktop OS to match their other OS product
    names (e.g., iOS, tvOS).  OS X will now be macOS.

--- macos/set-defaults.sh
+++ macos/set-defaults.sh
@@ -1,9 +1,9 @@
-# Sets reasonable OS X defaults.
+# Sets reasonable macOS defaults.
 #
-# Or, in other words, set shit how I like in OS X.
+# Or, in other words, set shit how I like in macOS.
 #
 # The original idea (and a couple settings) were grabbed from:
-#   https://github.com/mathiasbynens/dotfiles/blob/master/.osx
+#   https://github.com/mathiasbynens/dotfiles/blob/master/.macos
 #
 # Run ./set-defaults.sh and you'll be good to go.
 
@@ -159,3 +159,10 @@ defaults write com.apple.finder QLEnableTextSelection -bool true
 
 # Disable Photos.app launch
 defaults write com.apple.ImageCapture disableHotPlug -bool YES
+
+# Set up Safari for development.
+defaults write com.apple.Safari IncludeInternalDebugMenu -bool true
+defaults write com.apple.Safari IncludeDevelopMenu -bool true
+defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
+defaults write com.apple.Safari "com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled" -bool true
+defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
