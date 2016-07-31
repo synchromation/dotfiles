0dfd7b29439004dd38bad97cb3d800239b0f94c5
Denis Lebedev <d2.lebedev@gmail.com>
2016-07-31 21:56:10 +0100

    Add httpie

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -32,7 +32,7 @@ brew upgrade --all
 # Install homebrew packages
 echo "Installing packages."
 brew install cask chisel jq hub mergepbx swiftlint
-
+brew install httpie
 echo "Installing cask apps."
 export HOMEBREW_CASK_OPTS="--appdir=/Applications"
 
