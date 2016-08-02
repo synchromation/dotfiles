dcecddad45a753b4368c4db33e10cc44bfba3dc9
Denis Lebedev <d2.lebedev@gmail.com>
2016-08-02 22:23:51 +0100

    Brewfile

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -1,9 +1,4 @@
 #!/bin/sh
-#
-# Homebrew
-#
-# This installs some of the common dependencies needed (or at least desired)
-# using Homebrew.
 
 # Check for Homebrew
 if test ! $(which brew)
@@ -18,47 +13,8 @@ then
   then
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/linuxbrew/go/install)"
   fi
-  
+
 fi
 
-# Make sure we’re using the latest Homebrew.
-echo "Updating homebrew."
-brew update
-
-# Upgrade any already-installed formulae.
-echo "Upgrading homebrew packages."
-brew upgrade --all
-
-# Install homebrew packages
-echo "Installing packages."
-brew install cask chisel jq hub mergepbx swiftlint
-brew install httpie
-echo "Installing cask apps."
-export HOMEBREW_CASK_OPTS="--appdir=/Applications"
-
-APPS=(
-    todoist spectacle vlc viber spotify slack skype iterm2 appcleaner dropbox rescuetime
-    google-photos-backup daisydisk charles telegram ynab flux clipmenu anki nvalt atom
-    )
-for app in ${APPS[@]}; do
-    brew cask install $app
-done
-     
-#QuickLook plugins
-
-QLPLUGINS=(qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzipql qlimagesize webpquicklook provisionql)
-
-for plugin in ${QLPLUGINS[@]}; do
-    brew cask install $plugin
-done
-
-qlmanage -r
-
 #Cleanup old versions
-
-echo "Cleaning up..."
-brew cleanup
-brew cask cleanup
-
-echo "Done."
 exit 0
