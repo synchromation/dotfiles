1195dc65995cafa364f227597ae081b35f84b6e6
Denis Lebedev <d2.lebedev@gmail.com>
2016-08-14 21:40:26 +0100

    Properly configure vim to run in terminal

--- vim/aliases.zsh
+++ vim/aliases.zsh
@@ -1,2 +1,2 @@
 alias gvim='/Applications/MacVim.app/Contents/MacOS/Vim -g'
-export GIT_EDITOR='/Applications/MacVim.app/Contents/MacOS/Vim -g -f --nomru -c "au VimLeave * open . -a iTerm"'
+export GIT_EDITOR=vim
