e41703cd19287894a5f2ea5bfaffc6083bfeb4f6
Denis Lebedev <d2.lebedev@gmail.com>
2016-09-12 15:48:25 +0100

    Better removal of +/- from diffs and logs
    https://blog.twobucks.co/git-diff-tips-and-tricks/

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -11,7 +11,7 @@ alias gl='git pull --prune'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias gp='git push origin HEAD'
 # Remove `+` and `-` from start of diff lines; just rely upon color.
-alias gd='git diff --color | sed -E "s/^([^-+ ]*)[-+ ]/\\1/" | less -r'
+alias gd='git diff --color'
 alias gc='git commit'
 alias gca='git commit -a'
 alias gco='git checkout'
