51a4a07180d9d9520acbf1df8b6cbc9941add353
Denis Lebedev <d2.lebedev@gmail.com>
2016-10-16 22:06:09 +0100

    Hack to restore owerwritten zshrc

--- zsh/install.sh
+++ zsh/install.sh
@@ -1,2 +1,4 @@
 ## Install oh-my-zsh
 export ZSH="$HOME/oh-my-zsh"; sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+## Restore existing zsh config
+mv ~/.zshrc.pre-oh-my-zsh ~/.zshrc
