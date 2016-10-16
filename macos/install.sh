89e084acf159bf1a7e77dbfe528f8d98534558b6
Denis Lebedev <d2.lebedev@gmail.com>
2016-10-16 18:23:03 +0100

    Install MAS apps from command line

--- macos/install.sh
+++ macos/install.sh
@@ -1,3 +1,11 @@
+# Install AppStore apps with awesome mas CLI
+
+echo "› Installing MAS apps"
+mas install 907364780 # Pomodoro
+mas install 692867256 # Simplenote
+mas install 747648890 # Telegram
+mas install 425424353 # Unarchiver
+
 # The Brewfile handles Homebrew-based app and library installs, but there may
 # still be updates and installables in the Mac App Store. There's a nifty
 # command line interface to it that we can use to just install everything, so
