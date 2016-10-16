f21943463e2734d88fe9eecc48b8ab943bd7fadb
Denis Lebedev <d2.lebedev@gmail.com>
2016-10-16 19:30:25 +0100

    Enable iTerm custom preferences by default

--- macos/set-defaults.sh
+++ macos/set-defaults.sh
@@ -148,6 +148,7 @@ killall Dock
 
 # Set custom preferences location
 defaults write com.googlecode.iterm2 PrefsCustomFolder -string $DOT_FILES/iterm2
+defaults write com.googlecode.iterm2 LoadPreferencesFromCustomFolder -bool true
 
 # Disable animation
 defaults write com.googlecode.iterm2 HotkeyTermAnimationDuration -float 0.00001
