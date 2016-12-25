53efd2e93e52c73ce130787d1381d7264f7466cf
Denis Lebedev <d2.lebedev@gmail.com>
2016-12-25 19:54:43 +0000

    Xcode defaults

--- macos/set-defaults.sh
+++ macos/set-defaults.sh
@@ -7,8 +7,75 @@
 #
 # Run ./set-defaults.sh and you'll be good to go.
 
-# Disable Storyboards reopening after Xcode relaunch.
+#
+# Xcode
+#
+
+# Trim trailing whitespace
+defaults write com.apple.dt.Xcode DVTTextEditorTrimTrailingWhitespace -bool true
+
+# Trim whitespace only lines
+defaults write com.apple.dt.Xcode DVTTextEditorTrimWhitespaceOnlyLines -bool true
+
+# Don't show completion on escape
+defaults write com.apple.dt.Xcode DVTTextShowCompletionsOnEsc -bool false
+
+# Show line numbers
+defaults write com.apple.dt.Xcode DVTTextShowLineNumbers -bool true
+
+# Hide the code folding ribbon
+defaults write com.apple.dt.Xcode DVTTextShowFoldingSidebar -bool true
+
+# Enable automatic updates
+defaults write com.apple.dt.Xcode DVTDownloadableAutomaticUpdate -bool true
+
+# Live issues
+defaults write com.apple.dt.Xcode IDEEnableLiveIssues -bool true
+
+# Continue building after errors
+defaults write com.apple.dt.Xcode IDEBuildingContinueBuildingAfterErrors -bool true
+
+# Setup page guide
+defaults write com.apple.dt.Xcode DVTTextShowPageGuide -bool true
+defaults write com.apple.dt.Xcode DVTTextPageGuideLocation -int 140
+
+# Enable internal debug menu
+defaults write com.apple.dt.Xcode ShowDVTDebugMenu -bool true
+
+# Source control local revision side
+defaults write com.apple.dt.Xcode DVTComparisonOrientationDefaultsKey -int 0
+
+# Stack assitant editors vertically
+defaults write com.apple.dt.Xcode AssistantEditorsLayout -int 1
+
+# Use open quickly to open in the focused pane
+defaults write com.apple.dt.Xcode IDEEditorCoordinatorTarget_Click -string FocusedEditor
+
+# Disable source control
+defaults write com.apple.dt.Xcode IDESourceControlEnableSourceControl_7_1 -bool false
+
+# Show build times in toolbar
+# http://cocoa.tumblr.com/post/131023038113/build-speed
+defaults write com.apple.dt.Xcode ShowBuildOperationDuration -bool true
+
+# Add more information to Xcode's build output about why specific commands are being run
+# https://twitter.com/bdash/status/661742266487205888
+# http://www.openradar.me/27516128
+defaults write com.apple.dt.Xcode ExplainWhyBuildCommandsAreRun -bool true
+
+# Stop Xcode from reopening files (specifically storyboards) on launch.
+# I also have a `xcuser` shell command to wipe this state so you don't reopen
+# storyboards and dirty the diff, or just take 30 seconds to launch
 defaults write com.apple.dt.Xcode IDEDisableStateRestoration -bool true
+
+# Write detailed build system info into derived data
+# If you don't enable this but `mkdir /tmp/xcode_dependency_logs` the logs will
+# be created there instead
+defaults write com.apple.dt.Xcode EnableBuildSystemLogging -bool true
+
+#
+# Other
+#
 
 # Disable press-and-hold for keys in favor of key repeat.
 defaults write -g ApplePressAndHoldEnabled -bool false
